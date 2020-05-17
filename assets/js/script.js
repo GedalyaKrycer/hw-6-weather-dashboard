78950e9c1cd89350cb3d545d413e134970f95049
Gedalya Krycer <gedalya@krycer.com>
2020-05-16 21:08:33 -0700

    Added base api code for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 console.log("hello world")
+
+var owKey = "6b0f83d2f817a4a623181896ec6d38d0";
+var city = "Las Vegas"
+
+// ————————————————————————————————————————————————————————————————————
+// Current Weather
+// ————————————————————————————————————————————————————————————————————
+
+var curWeatherQueryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + owKey;
+
+console.log(curWeatherQueryUrl);
+
+$.ajax({
+    url: curWeatherQueryUrl,
+    method: "GET" 
+}).then(function(response){
+    console.log(response.name);
+}); 
+
