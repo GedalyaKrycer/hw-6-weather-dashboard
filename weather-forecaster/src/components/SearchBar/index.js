aee1a592b76ff0d9a885fcf9aa60d46c9b782cb4
Gedalya Krycer <gedalya@krycer.com>
2020-09-23 16:43:25 -0700

    Added initial search bar

--- weather-forecaster/src/components/SearchBar/index.js
+++ weather-forecaster/src/components/SearchBar/index.js
@@ -3,12 +3,26 @@ import './style.css';
 
 function SearchBar() {
     return (
-        <header className="columns">
-            <div className="column">
-                <h1>Weather Forecaster</h1>
-            </div>
-            <div className="column">
-                search bar
+        <header className="search-bar__header">
+            <div className="columns">
+                <div className="column is-one-fifth">
+                    <h1 className="search-bar__logo">Weather Forecaster</h1>
+                </div>
+                <div className="column">
+                    <div className="field has-addons">
+                        <div className="control is-expanded">
+                            <input id="citySearch" className="input" type="text" placeholder="Enter a location here…" />
+                        </div>
+                        <div className="control">
+                            <button id="citySubmit" type="submit" className="button g__full-percent-height">
+                                Search
+                        </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="column is-one-fifth">
+                    Github
+                </div>
             </div>
 
         </header>
