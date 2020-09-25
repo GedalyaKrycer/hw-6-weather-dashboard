5fbd35675de43a72c5dad3280aad17141a385450
Gedalya Krycer <gedalya@krycer.com>
2020-09-24 22:14:11 -0700

    Styles for 3 day forecst

--- weather-forecaster/src/components/CurrentTemp/index.js
+++ weather-forecaster/src/components/CurrentTemp/index.js
@@ -10,27 +10,28 @@ function CurrentTemp() {
                 <div className="current-temp__meta">
                     <p className="current-temp__meta-date">Sep / 23 / 2020</p>
                     <div className="current-temp__meta-status">
-                        <img src="https://openweathermap.org/img/wn/01d.png" alt="Weather status icon" />
+                        <img src="https://openweathermap.org/img/wn/01d.png" alt="Weather status icon"
+                        className="g__temp-status-img"/>
                         <p>Clear Sky</p>
                     </div>
                 </div>
             </div>
             <div className="columns current-temp__details-container">
                 <div className="column">
-                    <h3>97<sup className="current-temp__details-metric">°F</sup></h3>
-                    <p className="current-temp__details-label">Temperature</p>
+                    <h3>97<sup className="g__details-metric">°F</sup></h3>
+                    <p className="g__details-label">Temperature</p>
                 </div>
                 <div className="column">
-                    <h3>10<sup className="current-temp__details-metric">%</sup></h3>
-                    <p className="current-temp__details-label">Humidity</p>
+                    <h3>10<sup className="g__details-metric">%</sup></h3>
+                    <p className="g__details-label">Humidity</p>
                 </div>
                 <div className="column">
-                    <h3>8<sup className="current-temp__details-metric">MPH</sup></h3>
-                    <p className="current-temp__details-label">Wind Speed</p>
+                    <h3>8<sup className="g__details-metric">MPH</sup></h3>
+                    <p className="g__details-label">Wind Speed</p>
                 </div>
                 <div className="column">
                     <h3>7.28</h3>
-                    <p className="current-temp__details-label">UV Index</p>
+                    <p className="g__details-label">UV Index</p>
                 </div>
 
             </div>
--- weather-forecaster/src/components/FiveDayForecast/index.js
+++ weather-forecaster/src/components/FiveDayForecast/index.js
@@ -4,7 +4,73 @@ import './style.css';
 function FiveDayForecast() {
     return (
         <div>
-            
+            <h4>3 Day Forecast</h4>
+            <div className="columns forecast__main">
+                <div className="column">
+                    <div className="forecast__container">
+                        <h5>Sep / 25 / 2020</h5>
+                        <img
+                            src="https://openweathermap.org/img/wn/01d.png"
+                            alt="Weather status icon"
+                            className="g__temp-status-img" />
+                        <p>Clear Sky</p>
+
+                        <div className="forecast__meta-container">
+                            <h3>61<sup className="g__details-metric">°F</sup></h3>
+                            <p className="g__details-label">Temperature</p>
+                        </div>
+
+                        <div className="forecast__meta-container">
+                            <h3>20<sup className="g__details-metric">%</sup></h3>
+                            <p className="g__details-label">Humidity</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="forecast__container">
+                        <h5>Sep / 25 / 2020</h5>
+                        <img
+                            src="https://openweathermap.org/img/wn/01d.png"
+                            alt="Weather status icon"
+                            className="g__temp-status-img" />
+                        <p>Clear Sky</p>
+
+                        <div className="forecast__meta-container">
+                            <h3>61<sup className="g__details-metric">°F</sup></h3>
+                            <p className="g__details-label">Temperature</p>
+                        </div>
+
+                        <div className="forecast__meta-container">
+                            <h3>20<sup className="g__details-metric">%</sup></h3>
+                            <p className="g__details-label">Humidity</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="forecast__container">
+                        <h5>Sep / 25 / 2020</h5>
+                        <img
+                            src="https://openweathermap.org/img/wn/01d.png"
+                            alt="Weather status icon"
+                            className="g__temp-status-img" />
+                        <p>Clear Sky</p>
+
+                        <div className="forecast__meta-container">
+                            <h3>61<sup className="g__details-metric">°F</sup></h3>
+                            <p className="g__details-label">Temperature</p>
+                        </div>
+
+                        <div className="forecast__meta-container">
+                            <h3>20<sup className="g__details-metric">%</sup></h3>
+                            <p className="g__details-label">Humidity</p>
+                        </div>
+                    </div>
+                </div>
+         
+
+
+
+            </div>
         </div>
     )
 }
